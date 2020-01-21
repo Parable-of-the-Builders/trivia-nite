8085ae8c664aa00fe6e2d3edf39ddb44c6d5f602
Cuthbert20 <knowltonspencer@gmail.com>
2020-01-21 08:36:46 -0700

    setting up server

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const session = require("express-session");
+const { SERVER_PORT, SESSION_SECRET } = process.env;
+const authCtrl = require("./src/server/controller/authController");
+const apiCtrl = require("./src/server/controller/apiController");
+
+app.use(express.json());
+
+//Session
+// app.use(
+//   session({
+//     secret: SESSION_SECRET,
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: {
+//       maxAge: 1000 * 60 * 60 * 24 * 7
+//     }
+//   })
+// );
+
+//waiting on massive til needed
+
+app.listen(SERVER_PORT, () =>
+  console.log(`${SERVER_PORT} is up and listening`)
+);
