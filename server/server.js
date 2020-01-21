e5076c4d3c9f348614e0ad29049110e44522e6c2
Cuthbert20 <knowltonspencer@gmail.com>
2020-01-21 15:10:12 -0700

    running api test

--- server/server.js
+++ server/server.js
@@ -3,25 +3,29 @@ const app = express();
 require("dotenv").config();
 const session = require("express-session");
 const { SERVER_PORT, SESSION_SECRET } = process.env;
-const authCtrl = require("./src/server/controller/authController");
-const apiCtrl = require("./src/server/controller/apiController");
+const authCtrl = require("./controller/authController");
+const apiCtrl = require("./controller/apiController");
 
 app.use(express.json());
 
+app.get("/api/test", (req, res) => res.json("Test"));
+
 //Session
-// app.use(
-//   session({
-//     secret: SESSION_SECRET,
-//     resave: false,
-//     saveUninitialized: false,
-//     cookie: {
-//       maxAge: 1000 * 60 * 60 * 24 * 7
-//     }
-//   })
-// );
+app.use(
+  session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+  })
+);
+
+//api Endpoints
 
 //waiting on massive til needed
 
 app.listen(SERVER_PORT, () =>
-  console.log(`${SERVER_PORT} is up and listening`)
+  console.log(`${SERVER_PORT}  is up and listening`)
 );
