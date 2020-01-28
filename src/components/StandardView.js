1d3234a8641ecefa76b60902909900f7043a8e6e
Cuthbert20 <knowltonspencer@gmail.com>
2020-01-28 11:38:01 -0700

    displaying question and setting it to state

--- src/components/StandardView.js
+++ src/components/StandardView.js
@@ -1,24 +1,38 @@
-import React, {useEffect} from 'react'
-import {connect} from 'react-redux'
-import axios from 'axios'
-import {setQuestions} from '../ducks/reducer'
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import axios from "axios";
+import { setQuestions } from "../ducks/reducer";
+import "../styles/StandardView.scss";
 
-
-const StandardView = (props) => {
-    useEffect(() => {
-        axios.get('https://opentdb.com/api.php?amount=10&category=20&encode=base64').then(res => (
-            props.setQuestions(res.data)
-        ))
-    }, [])
-    console.log(props)
-    return(
-        <div>
-            <h1>This is the Standard View</h1>
-        </div>
-    )
-}
-const mapStateToProps = (redux) => {
-    const {questions} = redux
-    return questions
-}
-export default connect(mapStateToProps, {setQuestions})(StandardView)
+const StandardView = props => {
+  const [question, setQuestion] = useState("");
+  useEffect(() => {
+    axios
+      .get("https://opentdb.com/api.php?amount=1&category=20&encode=url3986")
+      .then(res => props.setQuestions(res.data.results));
+  }, []);
+  console.log(props.questions);
+  function getQuestion() {
+    setQuestion(props.questions[0].question);
+  }
+  console.log(question);
+  return (
+    <div className="StandarView-container">
+      <button onClick={getQuestion}>Get Question</button>
+      <main>
+        {question !== "" && <h3>{decodeURIComponent(question)}</h3>}
+        <h5>Catetory</h5>
+      </main>
+      <section>
+        <select name="" id="">
+          <option value="">Select an Answer</option>
+        </select>
+      </section>
+    </div>
+  );
+};
+const mapStateToProps = redux => {
+  const { questions } = redux;
+  return { questions };
+};
+export default connect(mapStateToProps, { setQuestions })(StandardView);
