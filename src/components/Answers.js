5997b8effdb06a2d35502e4efe5555a47130cafa
Cuthbert20 <knowltonspencer@gmail.com>
2020-02-03 12:52:45 -0700

    updated shuffle function so it worked correctly, Created Answer Component to display answers

---
+++ src/components/Answers.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Answers(props) {
+  console.log(props);
+  return (
+    <div>
+      <h1>Answers Component</h1>
+    </div>
+  );
+}
+
+export default Answers;
--- src/components/StandardView.js
+++ src/components/StandardView.js
@@ -3,26 +3,30 @@ import { connect } from "react-redux";
 import axios from "axios";
 import { setQuestions } from "../ducks/reducer";
 import "../styles/StandardView.scss";
+import Answers from "./Answers";
 
 const StandardView = props => {
-  const [question, setQuestion] = useState();
+  const [question, setQuestion] = useState([]);
   // useEffect(() => {
   //   getQuestion();
   // }, []);
   function getQuestion() {
     axios
-      .get("https://opentdb.com/api.php?amount=1&category=20&encode=url3986")
+      .get("https://opentdb.com/api.php?amount=1&encode=url3986")
       .then(res => setQuestion(res.data.results));
   }
 
   function shuffle(arr) {
-    let newIndex, temp;
-    for (let i = arr.length - 1; i > 0; i--) {
-      newIndex = Math.floor(Math.random * (i + 1));
-      temp = arr[i];
-      arr[i] = arr[newIndex];
-      arr[newIndex] = temp;
+    let ctr = arr.length;
+    let index, temp;
+    while (ctr > 0) {
+      index = Math.floor(Math.random() * ctr);
+      ctr--;
+      temp = arr[ctr];
+      arr[ctr] = arr[index];
+      arr[index] = temp;
     }
+
     return arr;
   }
   console.log(question);
@@ -32,11 +36,18 @@ const StandardView = props => {
       <main>
         {question &&
           question.map((val, index) => {
+            const rightAnswer = decodeURIComponent(val.correct_answer);
             let answers = [...val.incorrect_answers, val.correct_answer];
+            shuffle(answers);
             return (
               <div key={index}>
+                <h5>Category: {decodeURIComponent(val.category)}</h5>
                 <h3>{decodeURIComponent(val.question)}</h3>
-                <h5>{val.category}</h5>
+                <Answers
+                  answers={answers}
+                  getQuestion={getQuestion}
+                  rightAnswer={rightAnswer}
+                />
               </div>
             );
           })}
