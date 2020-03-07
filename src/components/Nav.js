ae459e3c80d6ce73c09489859b0635d4983233d8
Cuthbert20 <knowltonspencer@gmail.com>
2020-03-07 09:21:44 -0700

    added button and some styles

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Nav.scss";
+import axios from "axios";
 
 const Nav = () => {
+  async function getRes() {
+    const result = await axios.get(
+      `https://e4de2dea-abc7-48f5-bd0a-f8f9919e0ace.mock.pstmn.io/?method=country_code`
+    );
+    console.log(result.data);
+  }
   return (
     <nav className="nav-wrapper">
       <Link className="nav-link" to="/">
@@ -11,6 +18,7 @@ const Nav = () => {
       <Link className="nav-link" to="/standard-view">
         Free Play
       </Link>
+      <button onClick={() => getRes()}>Click me!</button>
     </nav>
   );
 };
